ed0edf111d5939a3b9fa04bda98e9f0b76d49e81
fleurkernevez <fleur@transparence.pro>
2025-04-08 12:12:29 +0200

    tests sur le counter précédemment retiré effacés

--- src/App.test.js
+++ src/App.test.js
@@ -7,14 +7,3 @@ test.skip('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('check counter on click me button', () => {
-  render(<App />);
-  const button = screen.getByRole('button');
-  const counter = screen.getByTestId('count')
-  expect(button).toBeInTheDocument();
-  expect(counter).toBeInTheDocument();
-  expect(counter).toHaveTextContent("0");
-  fireEvent.click(button);
-  expect(counter).toHaveTextContent("1");
- });
