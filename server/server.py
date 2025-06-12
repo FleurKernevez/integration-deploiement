6aca4c96184794f0bdaf50c3577275eeb537a3cd
fleurkernevez <fleur@transparence.pro>
2025-06-12 14:48:13 +0200

    fix docker compose issue

--- server/server.py
+++ server/server.py
@@ -14,9 +14,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+async def hello():
+    return {"message": "Hello, World!"}
+
 @app.get("/users")
 async def get_users():
+    try:
+        conn = mysql.connector.connect(
+            database=os.getenv("MYSQL_DATABASE"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_ROOT_PASSWORD"),
+            port=3306, 
+            host=os.getenv("MYSQL_HOST"))
+        cursor = conn.cursor()
+        sql_select_Query = "select * from utilisateur"
+        cursor.execute(sql_select_Query)
+        # get all records
+        records = cursor.fetchall()
+        print("Total number of rows in table: ", cursor.rowcount)
+        # renvoyer nos données et 200 code OK
+        return {'utilisateurs': records}
+    except Exception as e:
+        print(e)
+        print("An exception occurred")
     # Connexion à la base de données
+
+@app.post("/login")
+async def create_user(login: Login):
     conn = mysql.connector.connect(
         database=os.getenv("MYSQL_DATABASE"),
         user=os.getenv("MYSQL_USER"),
@@ -24,13 +49,17 @@ async def get_users():
         port=3306, 
         host=os.getenv("MYSQL_HOST"))
     cursor = conn.cursor()
-    sql_select_Query = "select * from utilisateur"
+    email = login.email
+    password = login.password
+    sql_select_Query = "select * from admin WHERE email=\""+ str(email) +"\" AND password=\""+ str(password)+"\";"
     cursor.execute(sql_select_Query)
     # get all records
     records = cursor.fetchall()
-    print("Total number of rows in table: ", cursor.rowcount)
-    # renvoyer nos données et 200 code OK
-    return {'utilisateurs': records}
+    if cursor.rowcount > 0:
+        encoded_jwt = jwt.encode({'data': [{'email':email}]}, MY_SECRET, algorithm=ALGORITHM)
+        return encoded_jwt
+    else:
+        raise Exception("Bad credentials")
 
 @app.post("/users")
 async def create_user(user: dict):
