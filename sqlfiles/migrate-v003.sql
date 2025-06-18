1b73f3c17c1afce83bba05eae1def56c6476cf53
fleurkernevez <fleur@transparence.pro>
2025-06-18 11:57:41 +0200

    régénération de la bdd

--- sqlfiles/migrate-v003.sql
+++ sqlfiles/migrate-v003.sql
@@ -1,4 +1,7 @@
-USE ynov_ci;
-SELECT * FROM utilisateur;
-INSERT INTO utilisateur (nom, prenom, email, dateDeNaissance, codePostal, ville) VALUES ('Dupont', 'Jean', 'jean.dupont@example.com', '1980-01-01', '75001', 'Paris');
-SELECT * FROM utilisateur;
+INSERT INTO utilisateur (nom, prenom, email, dateDeNaissance, codePostal, ville)
+SELECT * FROM (
+    SELECT 'Dupont', 'Jean', 'jean.dupont@example.com', '1980-01-01', '75001', 'Paris'
+) AS tmp
+WHERE NOT EXISTS (
+    SELECT email FROM utilisateur WHERE email = 'jean.dupont@example.com'
+) LIMIT 1;
--- sqlfiles/migrate-v004.sql
+++ sqlfiles/migrate-v004.sql
@@ -1,9 +1,12 @@
-USE ynov_ci;
-CREATE TABLE admin
-(
+CREATE TABLE IF NOT EXISTS admin (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     email VARCHAR(255),
     password VARCHAR(255)
 );
-INSERT INTO admin (email, password) VALUES ("loise.fenoll@ynov.com", "PvdrTAzTeR247sDnAZBr");
-DESCRIBE admin;
+
+-- Insère l'admin seulement s'il n'existe pas déjà
+INSERT INTO admin (email, password)
+SELECT * FROM (SELECT "loise.fenoll@ynov.com", "PvdrTAzTeR247sDnAZBr") AS tmp
+WHERE NOT EXISTS (
+    SELECT email FROM admin WHERE email = "loise.fenoll@ynov.com"
+) LIMIT 1;
