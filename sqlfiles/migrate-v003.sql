fad5851f4021c370c2fc8bd6a405af77ac8744fc
fleurkernevez <fleur@transparence.pro>
2025-06-18 14:49:43 +0200

    retour en arrière des fichiers sql

--- sqlfiles/migrate-v003.sql
+++ sqlfiles/migrate-v003.sql
@@ -1,7 +1,4 @@
-INSERT INTO utilisateur (nom, prenom, email, dateDeNaissance, codePostal, ville)
-SELECT * FROM (
-    SELECT 'Dupont', 'Jean', 'jean.dupont@example.com', '1980-01-01', '75001', 'Paris'
-) AS tmp
-WHERE NOT EXISTS (
-    SELECT email FROM utilisateur WHERE email = 'jean.dupont@example.com'
-) LIMIT 1;
+USE ynov_ci;Add commentMore actions
+SELECT * FROM utilisateur;
+INSERT INTO utilisateur (nom, prenom, email, dateDeNaissance, codePostal, ville) VALUES ('Dupont', 'Jean', 'jean.dupont@example.com', '1980-01-01', '75001', 'Paris');
+SELECT * FROM utilisateur;
