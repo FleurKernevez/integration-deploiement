868a9f6f3bbda8e1c69e5c53f011ccb5132db355
fleurkernevez <fleur@transparence.pro>
2025-06-23 10:51:50 +0200

    modification migrate

--- sqlfiles/migrate-v003.sql
+++ sqlfiles/migrate-v003.sql
@@ -1,4 +1,4 @@
-USE ynov_ci;Add commentMore actions
+USE ynov_ci;
 SELECT * FROM utilisateur;
 INSERT INTO utilisateur (nom, prenom, email, dateDeNaissance, codePostal, ville) VALUES ('Dupont', 'Jean', 'jean.dupont@example.com', '1980-01-01', '75001', 'Paris');
 SELECT * FROM utilisateur;
